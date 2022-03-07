f9755022dc865aa3d94b6f2330a6696c67fe2d24
Dmitry Mokrov <d.mokrov@just-work.org>
2022-03-07 15:17:36 +0300

    Add CRUD operations

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,10 +1,33 @@
 const { argsToArgsConfig } = require("graphql/type/definition");
 
 module.exports = {
-  newNote: async (parent, args, { models }) => {
+  newNote: async (parent, { content }, { models }) => {
     return await models.Note.create({
-      content: args.content,
+      content,
       author: 'Adam Scott',
     });
+  },
+  deleteNote: async (parent, { id }, { models }) => {
+    try {
+      await models.Note.findOneAndRemove({ _id: id });
+      return true;
+    } catch (err) {
+      return false;
+    }
+  },
+  updateNote: async (parent, { id, content }, { models }) => {
+    return await models.Note.findOneAndUpdate(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          content
+        }
+      },
+      {
+        new: true
+      }
+    );
   }
 }
--- src/schema.js
+++ src/schema.js
@@ -1,10 +1,13 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
+  scalar DateTime
   type Note {
     id: ID!
     content: String!
     author: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
   }
 
   type Query {
@@ -15,5 +18,7 @@ module.exports = gql`
 
   type Mutation {
     newNote(content: String!): Note!
+    updateNote(id: ID!, content: String!): Note!
+    deleteNote(id: ID!): Boolean!
   }
 `;
