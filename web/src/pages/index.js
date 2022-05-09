27e527bd2a69aa6c5f1a1c48cd34d08082c221d3
Dmitry Mokrov <d.mokrov@just-work.org>
2022-05-09 21:55:34 +0300

    Add private routes

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 
 import Layout from "../components/Layout";
 import HomePage from "./HomePage";
@@ -8,17 +9,39 @@ import NotePage from "./NotePage";
 import SignUpPage from "./SignUpPage";
 import SignInPage from "./SignInPage";
 
-export default function Pages() {
+const IS_LOGGED_IN_LOCAL = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+const PrivateRoute = () => {
+  let location = useLocation();
+  const { loading, error, data } = useQuery(IS_LOGGED_IN_LOCAL);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+  if (data.isLoggedIn) return <Outlet />;
+
+  return <Navigate to="/signin" state={{ from: location }} replace={true} />;
+};
+
+const Pages = () => {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} exact />
-        <Route path="/mynotes" element={<MyNotesPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/mynotes" element={<PrivateRoute />}>
+          <Route element={<MyNotesPage />} />
+        </Route>
+        <Route path="/favorites" element={<PrivateRoute />}>
+          <Route element={<FavoritesPage />} />
+        </Route>
         <Route path="/note/:id" element={<NotePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/signin" element={<SignInPage />} />
       </Routes>
     </Layout>
   );
-}
+};
+
+export default Pages;
